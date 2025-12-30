172ca7b5c30c868bd9fe88b22d45d6ebfa80ff33
Aljes Binkevich <binkevich.as@phystech.edu>
2025-12-30 14:35:08 +0000

    Fix import paths in __init__.py for dataset and preprocess modules

--- src/peak2vec/__init__.py
+++ src/peak2vec/__init__.py
@@ -1,5 +1,5 @@
-from src.peak2vec.dataset import PeakDataset, peak2vec_collate
-from src.peak2vec.preprocess import (
+from peak2vec.dataset import PeakDataset, peak2vec_collate
+from peak2vec.preprocess import (
     get_sampling_distributions,
     prepare_adata,
     get_sampling_distributions,
